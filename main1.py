c46e3fe43bf3859790252923642603d25671fc72
Yannys-coder <ccyannys123@gmail.com>
2024-09-13 11:45:25 -0400

    first commit

---
+++ main1.py
@@ -0,0 +1,13 @@
+fruits=["apples", "banana", "Grapes", "pear"]
+print(fruits[0:2])
+fruits.append("watermelom")
+print(fruits)
+fruits.remove("apples")
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.pop(2)
+print(fruits)
+fruits.reverse()
+print(fruits)
+
---
+++ main2.py
@@ -0,0 +1,10 @@
+dict={101:"great", 102:"appreciate", 103:"please", 104:"bad"}
+print(dict[103])
+
+dict[102]= "thankful"
+print(dict)
+
+dict[105]="appreciate"
+
+dict.pop(104)
+print(dict)
